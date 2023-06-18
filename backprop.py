ac57f0c7036f20e5a441580948e1dad67a56e50a
Swapnil Gusani <swapniel99@gmail.com>
2023-06-18 21:48:44 +0530

    bugfix

--- backprop.py
+++ backprop.py
@@ -134,7 +134,7 @@ class Test(object):
             image = self.test_incorrect_pred["images"][i].cpu()
             if denorm:
                 image = self.dataset.denormalise(image)
-            plt.imshow(self.dataset.show_transform(image))
+            plt.imshow(self.dataset.show_transform(image), cmap='gray')
             pred = str(self.test_incorrect_pred["predicted_vals"][i])
             truth = str(self.test_incorrect_pred["ground_truths"][i])
             if self.dataset.classes is not None:
