4fcb436a3451354f452b79b62dab2f56fae99906
Swapnil Gusani <swapniel99@gmail.com>
2023-06-19 00:58:09 +0530

    Update cifar10.py

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -11,10 +11,10 @@ class CIFAR10(DataSet):
 
     def get_train_loader(self):
         train_transforms = transforms.Compose([
-#             transforms.RandomHorizontalFlip(0.5),
-#             transforms.RandomGrayscale(0.1),
-#             transforms.RandomRotation((-7.0, 7.0)),
-#             transforms.RandomPerspective(0.3, 0.5),
+            transforms.RandomHorizontalFlip(0.5),
+            transforms.RandomGrayscale(0.1),
+            transforms.RandomRotation((-7.0, 7.0)),
+            transforms.RandomPerspective(0.3, 0.5),
             transforms.ToTensor(),
             transforms.Normalize(self.mean, self.std)
         ])
