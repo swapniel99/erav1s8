23a9ed13ba0e60273367ef28217ac0c5361dab73
Swapnil Gusani <swapniel99@gmail.com>
2023-06-18 20:02:54 +0530

    add s7 models

--- model.py
+++ model.py
@@ -131,27 +131,6 @@ class BaseModel(nn.Module):
         )
 
 
-class Model1(BaseModel):
-    def __init__(self):
-        super(Model1, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
-        self.fc1 = nn.Linear(4096, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.relu(self.conv3(x))
-        x = F.relu(F.max_pool2d(self.conv4(x), 2))
-        x = x.view(-1, 4096)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
-
-
 class Model2(BaseModel):
     def __init__(self):
         super(Model2, self).__init__()
